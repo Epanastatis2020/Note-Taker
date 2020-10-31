72d2d7c41de8e81d460131df3aeea47fa0c6d11a
ConA <65388616+Epanastatis2020@users.noreply.github.com>
2020-10-31 19:28:57 +1100

    added routes to htmlRoutes.js

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,19 @@
+//------------------------------------------------
+// Dependencies
+//------------------------------------------------
+
+const path = require("path");
+
+//------------------------------------------------
+// Setting routes
+//------------------------------------------------
+
+module.exports = function (app) {
+    app.get("/notes", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/notes.html"));
+    });
+
+    app.get("*", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
+};
